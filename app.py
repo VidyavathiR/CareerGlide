fa14152520033305f482316d18ecf83b1841e4a8
VidyavathiR <124717528+VidyavathiR@users.noreply.github.com>
2024-01-22 05:23:19 +0000

    added html and css

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 from flask import Flask
+from flask.templating import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def web():
-  return "hello vidya"
+  return render_template("home.html")
 
 
 if __name__ == "__main__":
