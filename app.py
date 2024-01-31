5ce398a69a07d024ddee76137c062cfbe8127b8c
VidyavathiR <124717528+VidyavathiR@users.noreply.github.com>
2024-01-31 06:46:38 +0000

    database connection

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 from flask import Flask, render_template, jsonify, request, redirect
-from database import load_jobs_from_db
+from database import load_jobs_from_db, load_job_from_db, add_application_to_db
 
 app = Flask(__name__)
-
 
 @app.route("/")
 def web():
@@ -14,6 +13,20 @@ def web():
 def listjobs():
   jobs = load_jobs_from_db()
   return jsonify(jobs)
+
+@app.route("/job/<id>")
+def show_jobs(id):
+  job = load_job_from_db(id)
+  if not job:
+    return "Not Found", 404
+  return render_template("jobpage.html", job=job)
+
+@app.route("/job/<id>/apply", methods=['POST'])
+def apply_to_job(id):
+  data = request.form
+  job = load_job_from_db(id)
+  add_application_to_db(id, data)
+  return render_template("application_submitted.html", application=data, job=job)
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -10,16 +10,49 @@ app.config['MYSQL_DB'] = "career"
 
 mysql = MySQL(app)
 
+
 def load_jobs_from_db():
-    with app.app_context():
-        cur = mysql.connection.cursor()
-        cur.execute("SELECT * FROM jobs")
-        columns = [desc[0] for desc in cur.description]  # Get column names
-        result = cur.fetchall()
-        # Convert result into a list of dictionaries
-        jobs = [dict(zip(columns, row)) for row in result]
-        # Print all rows in the form of dictionaries
-
-        return jobs
+  with app.app_context():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM jobs")
+    columns = [desc[0] for desc in cur.description]  # Get column names
+    result = cur.fetchall()
+    # Convert result into a list of dictionaries
+    jobs = [dict(zip(columns, row)) for row in result]
+    return jobs
 
 
+def load_job_from_db(job_id):
+  with app.app_context():
+    cur = mysql.connection.cursor()
+    query = "SELECT * FROM jobs WHERE id = %s"
+    cur.execute(query, (job_id, ))
+    result = cur.fetchone()
+
+    if result is None:
+      return None
+    else:
+      # return the result as a dictionary
+      columns = [desc[0] for desc in cur.description]
+      job_dict = dict(zip(columns, result))
+      return job_dict
+
+
+def add_application_to_db(job_id, data):
+  with app.app_context():
+    job_id = job_id
+    full_name = data['full_name']
+    email = data['email']
+    linkedin_url = data['linkedin_url']
+    education = data['education']
+    workexperience = data['workexperience']
+    resume_url = data['resume_url']
+    cur = mysql.connection.cursor()
+    cur.execute(
+        "INSERT INTO applications(job_id, full_name, email, linkedin_url, education, workexperience, resume_url) VALUES(%s, %s, %s, %s, %s, %s, %s)",
+        (job_id, full_name, email, linkedin_url, education, workexperience,
+         resume_url))
+    mysql.connection.commit()
+    cur.close()
+
+   
